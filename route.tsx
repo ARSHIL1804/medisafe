f4b7dc3bd8c47ad82ed9d9b20f7e0873ff850cf3
ARSHIL1804 <arshilvahora1804@gmail.com>
2024-10-15 22:42:55 +0530

    changes

--- route.tsx
+++ route.tsx
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import user from "@/lib/models/PatientSchema";
-import { formSchema } from "@/utils/utils";
 
 
 export async function POST(request: NextRequest) {
